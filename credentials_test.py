caf016691bb9aa6398df352ed2e12614a611d05b
nziokaivy <nziokaivy@gmail.com>
2019-01-26 18:42:01 +0300

    add setup method that runs before each test case

--- credentials_test.py
+++ credentials_test.py
@@ -3,10 +3,16 @@ from credentials import Credential
 
 class TestCredentials(unittest.TestCase):
     '''
-    Test class that defines test cases for the account user behaviours.
+    Test class that defines test cases for the credentials class behaviours.
 
     Args:
         unittest.TestCase: TestCase class that helps in creating tets cases
-    '''    
+    ''' 
+       
+    def setUp(self):
+        '''
+        Method to run before each test cases.
+        '''
+        self.new_account = Account_user("Ivy","Muffins01")
 
     
