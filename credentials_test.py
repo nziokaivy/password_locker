479f06cefbcf0bb647fd9d6d3af2e70bf71813dc
nziokaivy <nziokaivy@gmail.com>
2019-01-26 18:40:34 +0300

    Add test class that defines test cases for the credential class behaviour

--- credentials_test.py
+++ credentials_test.py
@@ -1,3 +1,12 @@
 import unittest
-from credentials import Credentials
+from credentials import Credential
 
+class TestCredentials(unittest.TestCase):
+    '''
+    Test class that defines test cases for the account user behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating tets cases
+    '''    
+
+    
