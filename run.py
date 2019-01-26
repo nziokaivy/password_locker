9ba637c6bc17ceef7a702fe2116fb765fd9e1b73
nziokaivy <nziokaivy@gmail.com>
2019-01-26 20:56:31 +0300

    add function to create a new account

--- run.py
+++ run.py
@@ -3,3 +3,10 @@
 import random
 from password import Account_user
 from credentials import Credential
+
+def create_new_credential(account_name, account_password):
+    """
+    Function to create a new account and its credentials
+    """
+    new_credential = Credential(account_name, account_password)
+    return new_credential
