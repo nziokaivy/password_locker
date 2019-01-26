2577eaa4ed5271ea7a8ca5399291cc3461c5b20e
nziokaivy <nziokaivy@gmail.com>
2019-01-26 09:40:01 +0300

    add first test

--- password_test.py
+++ password_test.py
@@ -9,9 +9,19 @@ class TestAccount_user(unittest.TestCase):
         unittest.TestCase: TestCase class that helps in creating tets cases
     '''    
 
-    defsetUp(self):
-    '''
-    Set up method to run before each test cases.
-    '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
     self.new_account = Account_user("Ivy","Muffins001")
-    
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_account.first_name,"Ivy")
+        self.assertEqual(self.new_account.password,"Muffins001")
+
+        if __name__ == '__main__':
+            unittest.main()
+
