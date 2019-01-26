1d9e9a673e5659fb48564e4fa62808681a4bf2f6
nziokaivy <nziokaivy@gmail.com>
2019-01-26 09:22:02 +0300

    add test class and generate its docstring

--- password_test.py
+++ password_test.py
@@ -1,2 +1,10 @@
 import unitttest
 from password import Account_user
+
+class TestAccount_user(unittest.TestCase):
+    '''
+    Test class that defines test cases for the account user behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating tets cases
+    '''    
