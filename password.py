5221b119551f5a366da41eecde081497e2f01dcc
nziokaivy <nziokaivy@gmail.com>
2019-01-26 09:01:00 +0300

    create new class user and generate docstring for it

--- password.py
+++ password.py
@@ -1,8 +1,28 @@
+import pyperclip
+import random
+class Account_user:
+    """
+    Class to create new user accounts and save information
+    """
+
+    users_list = []
+
+    # def__init__(self,first_name,password)
 class Password:
     """
     Class that generates new instances of passwords
     """
 
-    pass
+    def __init__(self,account_name,first_name,email_address,phone_number,password):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            account_name: New account name password is for.
+            _name : New contact last name.
+            number: New contact phone number.
+            email : New contact email address.
+        '''
 
 
