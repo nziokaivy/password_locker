b34ee55d32c6188f1be5e67dce7fb92ecf75f97e
nziokaivy <nziokaivy@gmail.com>
2019-01-27 09:06:19 +0300

    add function that finds credentials using account name

--- run.py
+++ run.py
@@ -10,3 +10,18 @@ def create_new_credential(account_name, account_password):
     """
     new_credential = Credential(account_name, account_password)
     return new_credential
+
+def save_new_credential(credentials):
+    """
+    Function to save the newly created account and password
+    """
+    credentials.save_credentials()
+
+def find_credential(account_name):
+    """
+    Function that finds credentials using the account_name provided
+    """
+    return Credential.find_by_name(account_name)
+
+    
+   
